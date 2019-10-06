0957742fcc0e469eb5511fc4e7e52e85e486c851
Thomas <thcarmonac@gmail.com>
2019-10-06 02:26:34 -0400

    Modelos agregados

--- eval/models.py
+++ eval/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Cuestionario(models.Model):
+    titulo = models.CharField(max_length=50, null=False, blank=False)
+    materia = models.CharField(max_length=50, null=True, blank=True)
+
+class Tipo(models.Model):
+    nombre = models.CharField(max_length=25, null= False, blank= False)
+
+class Pregunta(models.Model):
+    texto = models.CharField(max_length=50, null=False, blank= False)
+    id_tipo = models.ForeignKey(Tipo, on_delete= models.CASCADE)
+    id_cuestionario = models.ForeignKey(Cuestionario, on_delete=models.CASCADE)
+
+class Respuesta(models.Model):
+    texto = models.CharField(max_length=50, null=False, blank= False)
+    correcto = models.BooleanField(null=False, blank=False)
+    id_pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+
+
+
