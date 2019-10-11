aeb41d253d47e3fe6edb89a3ad7f04c26937781c
Diego <luisdiegoborja8@gmail.com>
2019-10-11 11:53:46 -0400

    Interfaz de cuestionario base agrgada

--- eval/models.py
+++ eval/models.py
@@ -5,13 +5,11 @@ class Cuestionario(models.Model):
     titulo = models.CharField(max_length=50, null=False, blank=False)
     materia = models.CharField(max_length=50, null=True, blank=True)
 
-class Tipo(models.Model):
-    nombre = models.CharField(max_length=25, null= False, blank= False)
-
 class Pregunta(models.Model):
     texto = models.CharField(max_length=50, null=False, blank= False)
     puntaje = models.PositiveSmallIntegerField(null=False, blank=False)
-    id_tipo = models.ForeignKey(Tipo, on_delete= models.CASCADE)
+    tipos =(('Falso y Verdadero','Falso y Verdadero'),('multiple opcion 1 respuesta','Multiple Opcion 1 Respuetsa'),('multiple opcion multiple respuesta','Multiple Opcion Multiple Respuesta'))
+    tipo = models.CharField(max_length=45, choices=tipos,null=False, blank= False)
     id_cuestionario = models.ForeignKey(Cuestionario, on_delete=models.CASCADE)
 
 class Respuesta(models.Model):
